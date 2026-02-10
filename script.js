7105a0dd445384c85dbaaa6896e7e96508ebd5c6
Code-fever1 <alijahinnovates@gmail.com>
2026-02-11 02:28:17 +0500

    ui improve

--- script.js
+++ script.js
@@ -232,8 +232,7 @@ function renderProjects() {
           <h3>${project.title}</h3>
           <p>${project.description}</p>
           <div class="project-actions">
-            <a class="btn btn-outline" href="${project.liveUrl}" target="_blank" rel="noopener noreferrer">Live Demo</a>
-            <a class="btn btn-primary" href="${project.sourceUrl}" target="_blank" rel="noopener noreferrer">Source</a>
+            <a class="btn btn-primary btn-soft" href="${project.sourceUrl}" target="_blank" rel="noopener noreferrer">Source</a>
           </div>
         </div>
       </article>
